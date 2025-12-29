0f9188b258fc64bb8321be3932dcce4593db4b11
Ayaal <uraankhayayaal@yandex.ru>
2025-12-29 10:08:25 +0300

    condition example

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,13 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
-
-extern crate rand;
-
 fn main() {
-    println!("Guess the number!");
+    let number = 3;
 
-    let secret_number = rand::rng().random_range(1..=100);
+    if number != 0 {
+        println!("Число не равно 0");
+    }
 
-    println!("The secret number is: {secret_number}");
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
+    if number < 5 {
+        println!("Число строго меньше пяти");
+    } else if number > 1 {
+        println!("Число больше единицы");
     }
 }
